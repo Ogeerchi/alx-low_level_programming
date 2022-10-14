a6b8651e0785ad51d7b865e0846bd0ce830b612a
Ogeerchi <kendendrey@gmail.com>
2022-10-14 15:36:10 -0700

    tsk 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - is the starting point of the program
+ * Return: value is zero
+ **/
+
+int main(void)
+{
+	/* we code */
+	char a;
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
