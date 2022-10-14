8d3ade4e7c89a770fa5f5fcebc107d9558566db9
Ogeerchi <kendendrey@gmail.com>
2022-10-14 16:02:05 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- program start point
+ * Return: this is a value of 0
+ **/
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == 'e' || i == 'q')
+			continue;
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
