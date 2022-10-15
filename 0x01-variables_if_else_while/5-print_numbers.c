4f95e88242e87d877f87bfeb8e2317b32d1b1cee
Ogeerchi <kendendrey@gmail.com>
2022-10-14 19:13:26 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - program startibg point
+ * Return: returns 0 value
+ **/
+
+int main(void)
+{
+	char i;
+
+		for (i = '0'; i <= '9'; i++)
+		{
+			putchar(i);
+		}
+		putchar('\n');
+		return (0);
+}
