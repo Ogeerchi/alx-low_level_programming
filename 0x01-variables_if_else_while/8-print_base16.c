b334d55795ead5f3135073b8cdab0bc2388012cb
Ogeerchi <kendendrey@gmail.com>
2022-10-14 19:48:20 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - program starting point
+ * Return: returns 0 value
+ **/
+
+int main(void)
+{
+	int i;
+	char hexvalues[] = "0123456789abcdef";
+
+	for (i = 0; i < 16; i++)
+	{
+		putchar(hexvalues[i]);
+	}
+	putchar('\n');
+	return (0);
+}
