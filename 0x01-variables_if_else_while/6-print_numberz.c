f80496128d6712a8ebc717dfaa8c5d870eeac94d
Ogeerchi <kendendrey@gmail.com>
2022-10-14 19:31:09 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main- progam starting point
+ * Return: returns a 0 value
+ **/
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
