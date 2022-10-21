41f2a3b07df5d4e1a42db8c4c8de24463b40c7ce
Ogeerchi <kendendrey@gmail.com>
2022-10-20 17:35:27 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main- draw a line
+ * @n: The number of lines to be drawn
+ * Return: zero
+ */
+
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
