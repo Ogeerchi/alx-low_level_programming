e9c0e9394fdf85817b8e73c8814a13ebfe522439
Ogeerchi <kendendrey@gmail.com>
2022-10-25 20:05:30 -0700

    task 00

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,24 +1,13 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - update the value of n
- * Return: success 0
+ * Return: n
  */
 
 void reset_to_98(int *n)
 {
-    int x;
-    int *p;
-
-    x = 402;
-    *p = &n;
-
-    putchar(x);
-
-    *p = 98;
-
-    putchar(x);
-    putchar('\n');
+    
+    *n = 98;
     
 }
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - swap values
+ *
+ * Return: success
+ */
+
+
